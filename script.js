1ad756df96b44acde264dadffc114739e80e812c
Scesar1 <scesar1@jhu.edu>
2022-09-06 15:43:23 -0400

    Add calculate functionality

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function handleOnClick() {
+const colors = ["eee2df","eed7c5","c89f9c","c97c5d","b36a5e", "eee2df"];
+
+function handleOnClickNow() {
   let output = document.querySelector(".output");
   output.style.display = "block";
 
@@ -12,6 +14,47 @@ function handleOnClick() {
   // calculate sleep cycles!
   for (let i = 1; i <= 6; i++) {
     now.setMinutes(now.getMinutes() + 90);
-    hours.innerHTML += now.toLocaleTimeString("en-US", { timeStyle: "short" }) + "<br />";
+    let element = document.createElement('div');
+    element.innerText = now.toLocaleTimeString("en-US", { timeStyle: "short" });
+    element.style.color = colors[i - 1];
+    hours.appendChild(element);
+
   } 
+}
+
+function handleOnClickCalculate() {
+  let output = document.querySelector(".outputCalc");
+  output.style.display = "block";
+
+  let hours = document.getElementById("hours2");
+  hours.innerHTML = "";
+
+  let selectedHours = document.getElementById("selectHours").value;
+  let selectedMinutes = document.getElementById("selectMinutes").value;
+  let selectedTime = document.getElementById("selectTime");
+
+  let timeVal = selectedTime.options[selectedTime.selectedIndex].text;
+
+  let time = new Date();
+  if (timeVal === "PM") {
+    selectedHours += 12;
+  }
+  time.setHours(selectedHours - 9);
+  time.setMinutes(selectedMinutes);
+  
+  // calculate sleep cycles!
+  for (let i = 1; i <= 4; i++) {
+    let element = document.createElement('div');
+    let cycleNumber = 7 - i;
+    let sleepHours = cycleConverter(cycleNumber);
+    element.innerText = time.toLocaleTimeString("en-US", { timeStyle: "short" }) + " For " 
+      + cycleNumber + " Cycles - " + sleepHours + " Hours of Sleep";
+    element.style.color = colors[i - 1];
+    hours.appendChild(element);
+    time.setMinutes(time.getMinutes() + 90);
+  } 
+}
+
+const cycleConverter = (cycle) => {
+  return (cycle * 90) / 60;
 }
